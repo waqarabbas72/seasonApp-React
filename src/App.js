f32207bb3b7d75a2af0a88b4956633f23077ef05
waqarabbas72 <sheraliate751@gmail.com>
2023-03-23 21:04:12 +0500

    seasonApp-React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent(){
+    if (this.state.errorMessage && !this.state.lat) {
+      return (
+        <div>
+          <h1>Error : {this.state.errorMessage}</h1>
+        </div>
+      );
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat ={this.state.lat}/>
+    }
+
+    return <Spinner message = "Please accept location request"/>
+  }
+  
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/SeasonDisplay.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './SeasonDisplay.css'
+
+// Seasons Dsiplay Function
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    console.log(season);
+    const text = season === 'Winter' ? "Burr , It's Chilly" : "Let's hit the beach";
+    const icon = season === 'Winter' ? "snowflake" : "sun"
+  
+    return (
+      <div className={`season-display ${season}`}>
+          <i className={`icon-left massive ${icon} icon`}/>
+          <h1>{text}</h1>
+          <i className={`icon-right massive ${icon} icon`}/>
+      </div>
+    );
+  };
+
+//  Get Seasons function
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "Summer" : "Winter";
+  } else {
+    return lat > 0 ? "Winter" : "Summer";
+  }
+};
+
+export default SeasonDisplay;
